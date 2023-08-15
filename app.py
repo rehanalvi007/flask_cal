a578f6526dfcdf3fb36355b2f58b2ac4a817fd44
“[rehan <rehanalvi8527@gmail.com>
2023-08-15 18:13:30 +0530

    code added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 # create object
 app = Flask(__name__)
 
@@ -9,16 +9,16 @@ def math_operator():
     number2=request.json['number2']
 
     if operation =="add":
-        result = number1 + number2
+        result = int(number1) + int(number2)
     elif operation=="multiply":
-        result = number1 * number2
+        result = int(number1) * int(number2)
     elif operation == "divide":
-        result = number1/number2
+        result = int(number1)/int(number2)
     elif operation == "subtract":
-        result = number1 - number2
+        result = int(number1) - int(number2)
     else:
         result = "incorrect number"
-    return result
+    return "the operation is {} and result is {}".format(operation,result)
 
 if __name__=='__main__':
     app.run() 
